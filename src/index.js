fa653346854bf2f53a5cd2f4f2002f38f0a97574
Elsa Gounot <elsa.gounot@gmail.com>
2018-04-09 17:12:43 +0200

    first step in react

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+// import Hello from './components/hello';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
